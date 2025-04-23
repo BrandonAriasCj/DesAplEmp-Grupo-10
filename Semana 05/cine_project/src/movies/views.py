c24581e8d7898ea31a6bc77ac8648a3b31729dc1
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-04-23 09:07:18 -0500

    preparar

--- Semana 05/cine_project/src/movies/views.py
+++ Semana 05/cine_project/src/movies/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+# peliculas/views.py
 
-# Create your views here.
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Muvie
+
+def lista_peliculas(request):
+    peliculas = Pelicula.objects.all()
+    return render(request, 'peliculas/lista.html', {'peliculas': peliculas})
