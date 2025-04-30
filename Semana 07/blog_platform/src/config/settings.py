728bf3f415206605adf5e6dea7d42e50c2f64035
Danna <danna.mendoza@tecsup.edu.pe>
2025-04-30 14:13:50 -0500

    add models

--- Semana 07/blog_platform/src/config/settings.py
+++ Semana 07/blog_platform/src/config/settings.py
@@ -37,6 +37,24 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'blog',  # Our blog app
+]
+
+# Add template directory configuration
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],  # Add this line
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
 
 MIDDLEWARE = [
