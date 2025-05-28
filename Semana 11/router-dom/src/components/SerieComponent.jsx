8e66f53c3b919373150612864f883fe00a45215f
Danna <danna.mendoza@tecsup.edu.pe>
2025-05-28 07:33:14 -0500

    añadiendo estilos y imagenes

--- Semana 11/router-dom/src/components/SerieComponent.jsx
+++ Semana 11/router-dom/src/components/SerieComponent.jsx
@@ -1,20 +1,33 @@
+import { useNavigate } from "react-router-dom"
 function SerieComponent(props) {
-    return (
-        <div className="card">
-            <img 
-                className="card-img-top" 
-                src={"https://dummyimage.com/400x250/000/fff&text="+props.imagen} 
-                alt="img" />
-            <div className="card-body">
-                <h5 className="card-title">{props.nombre}</h5>
-                <p className="card-text">{props.categoria}</p>
-                <div className="d-flex justify-content-between">
-                    <button className="btn btn-secondary">Editar</button> 
-                    <button className="btn btn-danger">Eliminar</button>
-                </div>
-            </div>
+  const navigate = useNavigate();
+  const gotoUrl = (codigo) => {
+    navigate("/series/edit/" + codigo);
+  };
+
+  // Detectar si imagen es base64 o nombre de archivo
+  const isBase64 = props.imagen?.startsWith("data:image");
+
+  return (
+    <div className="card">
+      <img
+        className="card-img-top"
+        src={isBase64 ? props.imagen : `/images/${props.imagen}`}
+        alt={props.nombre}
+      />
+
+      <div className="card-body">
+        <h5 className="card-title">{props.nombre}</h5>
+        <p className="card-text">{props.categoria}</p>
+        <div className="d-flex justify-content-between">
+          <button onClick={() => gotoUrl(props.codigo)} className="btn btn-secondary">
+            Editar
+          </button>
+          <button className="btn btn-danger">Eliminar</button>
         </div>
-    )
+      </div>
+    </div>
+  );
 }
-  
-export default SerieComponent
+
+export default SerieComponent;
