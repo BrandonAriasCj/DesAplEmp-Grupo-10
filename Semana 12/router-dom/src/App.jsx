dc84c2bdb24d4e58ef77e0a4b32f383e4d791269
Danna <danna.mendoza@tecsup.edu.pe>
2025-06-07 18:34:55 -0500

    primer commit

---
+++ Semana 12/router-dom/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import LoginPage from './pages/LoginPage.jsx'
+import HomePage from './pages/HomePage.jsx'
+import SeriePage from "./pages/SeriePage.jsx"
+import SerieFormPage from './pages/SerieFormPage.jsx'
+import CategoryPage from './pages/CategoryPage.jsx'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path= "/" element={<LoginPage/>}/>
+        <Route path= "/home" element={<HomePage/>}/>
+        <Route path= "/series" element={<SeriePage/>} />
+        <Route path= "/categories" element={<CategoryPage/>} />
+        <Route path= "/serie/edit/:idserie" element={<SerieFormPage/>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App
