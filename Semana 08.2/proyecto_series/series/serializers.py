a9e8295abdedf6ae9d81306609ad396499fbb658
Danna <danna.mendoza@tecsup.edu.pe>
2025-06-07 19:45:25 -0500

    adding serializers

---
+++ Semana 08.2/proyecto_series/series/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Category, Series
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ['cod', 'nom']
+
+class SeriesSerializer(serializers.ModelSerializer):
+    cat = serializers.SlugRelatedField(slug_field='nom', queryset=Category.objects.all())
+
+    class Meta:
+        model = Series
+        fields = ['cod', 'nom', 'cat', 'img']
