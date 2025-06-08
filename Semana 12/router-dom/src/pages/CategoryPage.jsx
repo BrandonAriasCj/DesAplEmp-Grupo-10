ff62d941669398b604ddc20edde197abfe26dd4a
Danna <danna.mendoza@tecsup.edu.pe>
2025-06-07 21:17:08 -0500

    changes

--- Semana 12/router-dom/src/pages/CategoryPage.jsx
+++ Semana 12/router-dom/src/pages/CategoryPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import {Link, useNavigate} from "react-router-dom";
 import axios from "axios";
 import HeaderComponent from "../components/HeaderComponent";
 import SerieComponent from "../components/SerieComponent";
