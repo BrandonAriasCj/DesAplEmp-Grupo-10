025bb14cf653aa0e78ae461437cf1960799514b1
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-06-07 19:36:14 -0500

    cambios

--- Semana 08.2/proyecto_series/series/models.py
+++ Semana 08.2/proyecto_series/series/models.py
@@ -1,15 +1,18 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=100)
+    cod = models.PositiveIntegerField(unique=True)
+    nom = models.CharField(max_length=100)
 
     def _str_(self):
-        return self.name
+        return self.nom
+
 
 class Series(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    category = models.ForeignKey(Category, related_name='series', on_delete=models.CASCADE)
+    cod = models.PositiveIntegerField(unique=True)
+    nom = models.CharField(max_length=200)
+    cat = models.ForeignKey(Category, related_name='series', on_delete=models.CASCADE)
+    img = models.CharField(max_length=200)
 
     def _str_(self):
-        return self.title
+        return self.nom
