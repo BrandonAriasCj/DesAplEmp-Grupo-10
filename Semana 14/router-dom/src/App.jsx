072a54fcfe122fbb3c350411506a1039eeba8de8
AxelLoayza <axtev2000@hotmail.com>
2025-06-18 11:53:41 -0500

    login page

--- Semana 14/router-dom/src/App.jsx
+++ Semana 14/router-dom/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { AppProvider } from './contexts/AppContext.jsx'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -17,15 +18,17 @@ function App() {
 
   return (
     <BrowserRouter>
+    <AppProvider>
       <Routes>
-        <Route path= "/" element={<LoginPage/>}/>
-        <Route path= "/home" element={<HomePage/>}/>
-        <Route path= "/series" element={<SeriePage/>} />
-        <Route path= "/categories" element={<CategoryPage/>} />
-        <Route path="/categories/new" element={<CategoryFormPage/>}/>
-        <Route path='/categories/edit/:cod' element={<CategoryEditFormPage />}/>
-        <Route path= "/series/new" element={<SerieFormPage/>} />
-      </Routes>
+          <Route path= "/" element={<LoginPage/>}/>
+          <Route path= "/home" element={<HomePage/>}/>
+          <Route path= "/series" element={<SeriePage/>} />
+          <Route path= "/categories" element={<CategoryPage/>} />
+          <Route path="/categories/new" element={<CategoryFormPage/>}/>
+          <Route path='/categories/edit/:cod' element={<CategoryEditFormPage />}/>
+          <Route path= "/series/new" element={<SerieFormPage/>} />
+        </Routes>
+      </AppProvider>
     </BrowserRouter>
   );
 }
