e0e53e6cc8fd49e9f4d4f85cabada6b8f401156a
Brandon Arias Cjuno <brandon12arias@gmail.com>
2025-06-18 11:56:29 -0500

    imortar views

--- Semana 08.2/proyecto_series/series/urls.py
+++ Semana 08.2/proyecto_series/series/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import CategoryViewSet, SeriesViewSet
+from series import views
 
 router = DefaultRouter()
 router.register(r'categories', CategoryViewSet)
